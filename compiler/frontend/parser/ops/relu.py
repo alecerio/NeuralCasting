dea048d2da9f6344c6d9e21e976fa5dbfd65f444
alecerio <alessandrocerioliphd@gmail.com>
2023-10-22 21:40:26 +0200

    add comments to relu

--- compiler/frontend/parser/ops/relu.py
+++ compiler/frontend/parser/ops/relu.py
@@ -1,3 +1,9 @@
+"""
+Author: Alessandro Cerioli
+Description:
+    The class ReLu represents the ReLU (Rectified Linear Unit) activation function.
+"""
+
 from compiler.frontend.parser.node.op_node import OpNode
 
 class ReLu(OpNode):
@@ -5,5 +11,11 @@ class ReLu(OpNode):
         super().__init__(name)
     
     def generate_code(self) -> str:
+        """
+        Method to generate the code related to the ReLu function.
+
+        Return:
+            The code related to the ReLu function.
+        """
         # TO DO
         return ""
