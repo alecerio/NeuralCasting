5bd546cb95b8b2806d7d439b4fdb93084b1c8051
alecerio <alessandrocerioliphd@gmail.com>
2023-10-22 12:35:23 +0200

    move torch2onnx to frontend subfolder

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,7 +1,8 @@
 import hydra
 import numpy as np
 import torch.nn as nn
-from compiler.frontend.torch2onnx import torch2onnx
+from compiler.frontend.torch2onnx.torch2onnx import torch2onnx
+from compiler.frontend.parser import parse
 
 def run(config, model, dummy_input):
     print("run compiler ...")
@@ -12,4 +13,6 @@ def run(config, model, dummy_input):
         path = temp_path + name + '.onnx'
         verbose : bool = bool(fr.verbose)
         torch2onnx(model, dummy_input, path, verbose)
-    
+    else:
+        raise Exception("Error: unexpected framework")
+    parse(config)
