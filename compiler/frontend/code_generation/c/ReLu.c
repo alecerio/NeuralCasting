971bc90a73c2a0281059f4e33dcec60c8cf078bd
alecerio <alessandrocerioliphd@gmail.com>
2023-11-03 10:20:02 +0100

    implementation of debug code for relu

--- compiler/frontend/code_generation/c/ReLu.c
+++ compiler/frontend/code_generation/c/ReLu.c
@@ -12,3 +12,12 @@ for(int i=0; i<$INPUT_SIZE; i++) {
         tensor_$OUTPUT_NAME[i * $BATCH_SIZE + j] = tensor_$INPUT_NAME[i * $BATCH_SIZE + j] > 0.0f ? tensor_$INPUT_NAME[i * $BATCH_SIZE + j] : 0.0f;
     }
 }
+
+#ifdef COMPILER_DEBUG
+printf("----------------- DEBUG OUTPUT $NAME -----------------\n");
+for(int i=0; i<$INPUT_SIZE; i++) {
+    printf("%f ", tensor_$OUTPUT_NAME[i]);
+}
+printf("\n");
+printf("------------------------------------------------------\n\n");
+#endif
