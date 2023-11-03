3c340792ee5f2d0ea010b8149685ebc108b0f5aa
alecerio <alessandrocerioliphd@gmail.com>
2023-11-03 09:58:22 +0100

    complete system to load parameters to the model

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -6,12 +6,16 @@ from compiler.frontend.parser.parser.parser import parse
 from compiler.frontend.parser.node.node import Node
 from compiler.frontend.parser.parser.dag import DAG
 
-def run(config, model, dummy_input):
+def run(config, model, dummy_input, params = None):
     print("run compiler ...")
 
     # lower from python framework to onnx
     fr = config.framework
     if(fr.framework_name == 'pytorch'):
+        # load parameters to the model
+        model.load_state_dict(params)
+
+        # create onnx file
         temp_path : str = str(fr.temp_path)
         name : str = str(fr.name)
         path = temp_path + name + '.onnx'
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import hydra
 from compiler.compiler import run
 from examples.neural_networks.pytorch.dummy_nn import DummyNN
@@ -12,15 +13,23 @@ def main(config):
     #model = DummyNN2(2, 3)
     dummy_input = torch.randn(1, 2)
 
-    input0 = torch.ones(1, 2)
-    output0 = model(input0)
-    print("Output: ", output0)
+    curr_file = os.path.abspath(__file__)
+    curr_path = os.path.dirname(curr_file)
+    params_path = curr_path + '/examples/params/pytorch/'
+    params_path += 'dummy_nn_params.pth'
+    #torch.save(model.state_dict(), params_path)
+
+    #input0 = torch.ones(1, 2)
+    #output0 = model(input0)
+    #print("Output: ", output0)
+
+    params = torch.load(params_path)
 
     #state_dict = model.state_dict()
     #for name, param in state_dict.items():
     #    print(f"Parameter: {name}, Size: {param.tolist()}")
 
-    run(config, model, dummy_input)
+    run(config, model, dummy_input, params)
 
 if __name__ == "__main__":
     main()
