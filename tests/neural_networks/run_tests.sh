3d6232e6aa24eddabbfaa41b8180a3d5d99df318
alecerio <alessandrocerioliphd@gmail.com>
2023-11-11 12:01:31 +0100

    implementation tests for fc-relu-fc-relu

--- tests/neural_networks/run_tests.sh
+++ tests/neural_networks/run_tests.sh
@@ -1,3 +1,17 @@
 tests_dir=$1
 
+# run test fc_relu
+echo ""
+echo "#####################################################################"
+echo "                  RUN TESTS FOR FC-RELU                              "
+echo "#####################################################################"
+echo ""
 python $tests_dir/neural_networks/fc_relu/main_test.py
+
+# run test fc_relu_fc_relu
+echo ""
+echo "#####################################################################"
+echo "               RUN TESTS FOR FC-RELU-FC-RELU                         "
+echo "#####################################################################"
+echo ""
+python $tests_dir/neural_networks/fc_relu_fc_relu/main_test.py
