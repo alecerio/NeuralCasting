32642a651fa8355fac8de25f6d00bd18b6ddeb6a
alecerio <alessandrocerioliphd@gmail.com>
2023-11-11 12:24:07 +0100

    implementation of tests for fc-sigmoid

--- tests/neural_networks/run_tests.sh
+++ tests/neural_networks/run_tests.sh
@@ -15,3 +15,11 @@ echo "               RUN TESTS FOR FC-RELU-FC-RELU                         "
 echo "#####################################################################"
 echo ""
 python $tests_dir/neural_networks/fc_relu_fc_relu/main_test.py
+
+# run test fc_sigmoid
+echo ""
+echo "#####################################################################"
+echo "                    RUN TESTS FOR FC-SIGMOID                         "
+echo "#####################################################################"
+echo ""
+python $tests_dir/neural_networks/fc_sigmoid/main_test.py
