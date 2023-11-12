032595790e72afd1b692144b53ccd88ce48ab81f
alecerio <alessandrocerioliphd@gmail.com>
2023-11-12 13:13:07 +0100

    generalize relu operator

--- compiler/frontend/code_generation/c/ReLu.c
+++ compiler/frontend/code_generation/c/ReLu.c
@@ -7,9 +7,9 @@ float tensor_$OUTPUT_NAME[$INPUT_SIZE];
 #undef CONNECTED_OUTPUT
 #endif
 
-for(int i=0; i<$INPUT_SIZE; i++) {
-    tensor_$OUTPUT_NAME[i] = tensor_$INPUT_NAME[i] > 0.0f ? tensor_$INPUT_NAME[i] : 0.0f;
-}
+$FOR_LOOPS_BEGIN
+tensor_$OUTPUT_NAME[$INDEX] = tensor_$INPUT_NAME[$INDEX] > 0.0f ? tensor_$INPUT_NAME[$INDEX] : 0.0f;
+$FOR_LOOPS_END
 
 #ifdef COMPILER_DEBUG
 printf("----------------- DEBUG OUTPUT $NAME -----------------\n");
--- compiler/frontend/parser/ops/relu.py
+++ compiler/frontend/parser/ops/relu.py
@@ -12,6 +12,7 @@ from compiler.frontend.parser.node_types.node_type import NodeType
 from compiler.frontend.parser.node_types.tensor_type import TensorType
 from compiler.frontend.common.common import fix_identifier
 from compiler.frontend.exceptions.CompilerException import CompilerException
+import math
 
 class ReLu(OpNode):
     def __init__(self, name : str):
@@ -33,6 +34,11 @@ class ReLu(OpNode):
         name : str = fix_identifier(self._name)
         input_name : str = fix_identifier(self._input_varnames[0])
         output_name : str = fix_identifier(self._output_varnames[0])
+        in_shape : int = self.infer_output_shape()
+        in_size : int = math.prod(in_shape)
+        for_loop_begin : str = self._gen_for_loop_begin(in_shape)
+        for_loop_end : str = self._gen_for_loop_end(in_shape)
+        index : str = self._gen_for_loop_index(in_shape)
 
         code : str = self._read_template_c("ReLu.c")
 
@@ -40,7 +46,10 @@ class ReLu(OpNode):
         code = self._expand_pattern(code, "$NAME", name)
         code = self._expand_pattern(code, "$INPUT_NAME", input_name)
         code = self._expand_pattern(code, "$OUTPUT_NAME", output_name)
-        code = self._expand_pattern(code, "$INPUT_SIZE", str(shape[1]))
+        code = self._expand_pattern(code, "$INPUT_SIZE", str(in_size))
+        code = self._expand_pattern(code, "$FOR_LOOPS_BEGIN", for_loop_begin)
+        code = self._expand_pattern(code, "$FOR_LOOPS_END", for_loop_end)
+        code = self._expand_pattern(code, "$INDEX", index)
 
         return code
     
@@ -76,3 +85,33 @@ class ReLu(OpNode):
     
     def generate_includes_code_c(self) -> str:
         return ""
+    
+    def _gen_for_loop_begin(self, shape : list[int]) -> str:
+        code : str = ""
+        n_dims = len(shape)
+        for dim in range(n_dims):
+            size : int = shape[dim]
+            index : str = "i" + str(dim)
+            code += "for(" + \
+                    "int " + index + "=0; " + index + "<" + str(size) + "; " + index + "++) {\n"
+        return code
+    
+    def _gen_for_loop_end(self, shape : list[int]) -> str:
+        code : str = ""
+        n_dims = len(shape)
+        for _ in range(n_dims):
+            code += "}\n"
+        return code
+    
+    def _gen_for_loop_index(self, shape : list[int]) -> str:
+        code : str = ""
+        n_dims : int = len(shape)
+        for i in range(n_dims):
+            index : str = "i" + str(i)
+            size : int = 1
+            for j in range(i+1, n_dims):
+                size *= shape[j]
+            code += index + "*" + str(size)
+            if i < n_dims-1:
+                code += " + "
+        return code
