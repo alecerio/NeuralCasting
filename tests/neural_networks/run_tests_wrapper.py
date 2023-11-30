99caf001955a56d7084570c235bd54782c5c4be2
alecerio <alessandrocerioliphd@gmail.com>
2023-11-30 16:49:49 +0100

    re-design tests structure

--- tests/neural_networks/run_tests_wrapper.py
+++ tests/neural_networks/run_tests_wrapper.py
@@ -1,9 +1,10 @@
-import subprocess
 from neural_cast.frontend.common.common import CompilerConfig
 import os
 import yaml
+from tests.neural_networks.constant.main_test import TestConstant
+import unittest
 
-def main():
+def run_neural_network_tests():
     curr_file = os.path.abspath(__file__)
     curr_path = os.path.dirname(curr_file)
     with open(curr_path + '/../../config/config.yaml', 'r') as yaml_file:
@@ -13,11 +14,7 @@ def main():
     test_path : str = CompilerConfig()['test_path']
     run_tests_path : str = test_path + "neural_networks/"
     
-    # run command
-    try:
-        subprocess.run(["bash", run_tests_path + "run_tests.sh", test_path])
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e}")
-
-if __name__ == "__main__":
-    main()
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(unittest.makeSuite(TestConstant))
+    test_runner = unittest.TextTestRunner(verbosity=2)
+    result = test_runner.run(test_suite)
