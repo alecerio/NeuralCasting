24910c8e1aa1ad08c28681c6967461864b356648
alecerio <alessandrocerioliphd@gmail.com>
2023-11-30 19:33:13 +0100

    update test reimplemented gru

--- tests/neural_networks/run_tests_wrapper.py
+++ tests/neural_networks/run_tests_wrapper.py
@@ -12,6 +12,7 @@ from tests.neural_networks.fc_sub.main_test import TestFcSub
 from tests.neural_networks.fc_tanh.main_test import TestFcTanh
 from tests.neural_networks.gather.main_test import TestGather
 from tests.neural_networks.matmul.main_test import TestMatMul
+from tests.neural_networks.reimplemented_gru.main_test import TestReimplementedGRU
 
 def run_neural_network_tests():
     curr_file = os.path.abspath(__file__)
@@ -31,5 +32,6 @@ def run_neural_network_tests():
     test_suite.addTest(unittest.makeSuite(TestFcTanh))
     test_suite.addTest(unittest.makeSuite(TestGather))
     test_suite.addTest(unittest.makeSuite(TestMatMul))
+    test_suite.addTest(unittest.makeSuite(TestReimplementedGRU))
     test_runner = unittest.TextTestRunner(verbosity=2)
     result = test_runner.run(test_suite)
