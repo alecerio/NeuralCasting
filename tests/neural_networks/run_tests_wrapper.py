74d8f3f2457f2438ae9491327903e25b8051a96f
alecerio <alessandrocerioliphd@gmail.com>
2023-11-30 17:50:12 +0100

    update test fc-mul

--- tests/neural_networks/run_tests_wrapper.py
+++ tests/neural_networks/run_tests_wrapper.py
@@ -3,6 +3,7 @@ import os
 import yaml
 from tests.neural_networks.constant.main_test import TestConstant
 from tests.neural_networks.fc_add.main_test import TestFcAdd
+from tests.neural_networks.fc_mul.main_test import TestFcMul
 import unittest
 
 def run_neural_network_tests():
@@ -15,5 +16,6 @@ def run_neural_network_tests():
     test_suite = unittest.TestSuite()
     test_suite.addTest(unittest.makeSuite(TestConstant))
     test_suite.addTest(unittest.makeSuite(TestFcAdd))
+    test_suite.addTest(unittest.makeSuite(TestFcMul))
     test_runner = unittest.TextTestRunner(verbosity=2)
     result = test_runner.run(test_suite)
