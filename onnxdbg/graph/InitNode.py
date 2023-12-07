55916d9a84e3d2e99c4175179d6272c7181f2fd0
alecerio <alessandrocerioliphd@gmail.com>
2023-12-07 17:37:39 +0100

    implementation of initializer node

---
+++ onnxdbg/graph/InitNode.py
@@ -0,0 +1,17 @@
+import GraphNode
+import onnx
+
+class InitNode(GraphNode):
+    def __init__(self, initializer : model.graph.initializer):
+        self._initializer = initializer
+
+    def __str__(self):
+        return super().__str__() + "\n" + \
+                "initializer: " + str(self._initializer)
+    
+    def set_initializer(self, initializer : model.graph.initializer):
+        self._initializer = initializer
+    
+    def get_initializer(self) -> model.graph.initializer:
+        return self._initializer
+                         
