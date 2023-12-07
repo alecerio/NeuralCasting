3193b2247defaebe50c2dcc00369ef253aa2c85a
alecerio <alessandrocerioliphd@gmail.com>
2023-12-07 17:32:14 +0100

    add name to onnx graph

--- onnxdbg/graph/GraphNode.py
+++ onnxdbg/graph/GraphNode.py
@@ -1,9 +1,15 @@
 from abc import ABC, abstractmethod
 
 class GraphNode(ABC):
-    def __init__(self):
-        pass
+    def __init__(self, name : str):
+        self._name : str = name
 
     def __str__(self):
-        pass
+        return "name: " + self._name
+    
+    def get_name(self) -> str:
+        return self._name
+    
+    def set_name(self, name : str) -> None:
+        self._name = name
 
