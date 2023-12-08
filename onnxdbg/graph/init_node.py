5e4c6661226f0b765b996ae9dc978bdb52739e83
alecerio <alessandrocerioliphd@gmail.com>
2023-12-08 12:06:49 +0100

    correction init and input types

--- onnxdbg/graph/init_node.py
+++ onnxdbg/graph/init_node.py
@@ -2,16 +2,16 @@ from graph_node import GraphNode
 import onnx
 
 class InitNode(GraphNode):
-    def __init__(self, initializer : model.graph.initializer):
-        self._initializer = initializer
+    def __init__(self, initializer : onnx.onnx_ml_pb2.TensorProto):
+        self._initializer : onnx.onnx_ml_pb2.TensorProto = initializer
 
     def __str__(self):
         return super().__str__() + "\n" + \
                 "initializer: " + str(self._initializer)
     
-    def set_initializer(self, initializer : model.graph.initializer):
+    def set_initializer(self, initializer : onnx.onnx_ml_pb2.TensorProto):
         self._initializer = initializer
     
-    def get_initializer(self) -> model.graph.initializer:
+    def get_initializer(self) -> onnx.onnx_ml_pb2.TensorProto:
         return self._initializer
                          
--- onnxdbg/graph/input_node.py
+++ onnxdbg/graph/input_node.py
@@ -2,16 +2,16 @@ from graph_node import GraphNode
 import onnx
 
 class InputNode(GraphNode):
-    def __init__(self, name : str, input : model.graph.input):
+    def __init__(self, name : str, input : onnx.onnx_ml_pb2.ValueInfoProto):
         super().__init__(name)
-        self._input = input
+        self._input : onnx.onnx_ml_pb2.ValueInfoProto = input
     
     def __str__(self):
         return super().__str__() + "\n" + \
                 "input: " + str(self._input)
     
-    def set_input(self, input : model.graph.input) -> None:
+    def set_input(self, input : onnx.onnx_ml_pb2.ValueInfoProto) -> None:
         self._input = input
     
-    def get_input(self) -> model.graph.input:
+    def get_input(self) -> onnx.onnx_ml_pb2.ValueInfoProto:
         return self._input
