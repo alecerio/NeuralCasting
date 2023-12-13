177abf7dfced6c7e95448e14b6b1d87c10c5cfd1
alecerio <alessandrocerioliphd@gmail.com>
2023-12-13 19:21:45 +0100

    add outputs to init node

--- onnxdbg/graph/init_node.py
+++ onnxdbg/graph/init_node.py
@@ -5,6 +5,7 @@ class InitNode(GraphNode):
     def __init__(self, name : str, initializer : onnx.onnx_ml_pb2.TensorProto):
         super().__init__(name)
         self._initializer : onnx.onnx_ml_pb2.TensorProto = initializer
+        self._outputs : list[GraphNode] = []
 
     def __str__(self):
         return super().__str__() + "\n" + \
