e7def05e5ebe7822dac1572f87cb4c55abf66163
alecerio <alessandrocerioliphd@gmail.com>
2023-12-16 00:22:07 +0100

    implementation of help command

--- onnxdbg/commands.py
+++ onnxdbg/commands.py
@@ -7,8 +7,12 @@ from onnxdbg.graph.output_node import OutputNode
 from onnxdbg.graph.op_node import OpNode
 from onnxdbg.utils import create_graph, inference_onnx_runtime
 
+onnxdbg_copy_args : list[str] = ['src', 'dst', 'mdl']
+onnxdbg_subgraph_args : list[str] = ['src', 'dst', 'mdl', 'out']
+onnxdbg_inferdbg_args : list[str] = ['srcp', 'dstp', 'mdl', 'input']
+
 def onnxdbg_copy(**kwargs):
-    args : list[str] = ['src', 'dst', 'mdl']
+    args : list[str] = onnxdbg_copy_args
 
     if not _check_valid_arguments(args, **kwargs):
         return
@@ -20,7 +24,7 @@ def onnxdbg_copy(**kwargs):
     graph.export_onnx_file(mdl, dst)
 
 def onnxdbg_subgr(**kwargs):
-    args : list[str] = ['src', 'dst', 'mdl', 'out']
+    args : list[str] = onnxdbg_subgraph_args
 
     if not _check_valid_arguments(args, **kwargs):
         return
@@ -36,7 +40,7 @@ def onnxdbg_subgr(**kwargs):
     onnx.save(model, dst)
 
 def onnxdbg_inferdbg(**kwargs):
-    args : list[str] = ['srcp', 'dstp', 'mdl', 'input']
+    args : list[str] = onnxdbg_inferdbg_args
 
     if not _check_valid_arguments(args, **kwargs):
         return
@@ -86,6 +90,59 @@ def onnxdbg_inferdbg(**kwargs):
     with open(pickle_output_shape_file, "wb") as pkl_file:
         pickle.dump(dict_output_shape, pkl_file)
 
+def onnxdbg_help(**kwargs) -> None:
+    nargs : int = len(kwargs.keys())
+    if nargs != 0:
+        print("No arguments expected for help")
+        return
+    
+    _print_help_header()
+
+    _print_command(
+        'copy',
+        'This command allows to copy an onnx file from a source directory to a destionation directory.\n'+
+        'The file is not simply copied, but the whole graph is re-created.',
+        onnxdbg_copy_args,
+        ['source onnx file', 'destination onnx file', 'onnx model name'],
+        "src='/home/workdir_source/my_model.onnx'\n" +
+        "dst='/home/workdir_destination/my_model.onnx'\n" + 
+        "mdl='model_onnx'"
+    )
+
+    _print_command(
+        'help',
+        'This command prints the available commands of onnxdbg.\n',
+        [],
+        [],
+        ""
+    )
+
+    _print_command(
+        'inferdbg',
+        'This command creates a sub-graph for possible operation node and output node and it runs the inference of the sub-graph.\n' +
+        'It provides, given an input, the output and the output shape for each node in the onnx file, this makes it perfect for debugging\n' +
+        'The results are returned in form of pkl files.',
+        onnxdbg_inferdbg_args,
+        ['source path', 'destination path', 'onnx model name', 'input dictionary for the inference'],
+        "src='/home/workdir_source/'\n" +
+        "dst='/home/workdir_destination/'\n" + 
+        "mdl='model_onnx'\n" +
+        "input={input1: [0, 0, 0], input2: [1, 1, 1, 1]}"
+    )
+
+    _print_command(
+        'subgr',
+        'This command allows to create a new onnx file that is a sub-graph of the original file.\n',
+        onnxdbg_subgraph_args,
+        ['source onnx file', 'destination sub-graph onnx file', 'onnx model name', 'name of onnx node to use as new output'],
+        "src='/home/workdir_source/my_model.onnx'\n" +
+        "dst='/home/workdir_destination/my_sub_model.onnx'\n" + 
+        "mdl='sub_model_onnx'\n" +
+        "out=/Tanh"
+    )
+
+    _print_help_tail()
+
 def _check_valid_arguments(args : list[str], **kwargs) -> bool:
     keys = kwargs.keys()
     for key in keys:
@@ -93,3 +150,27 @@ def _check_valid_arguments(args : list[str], **kwargs) -> bool:
             print("Argument '" + key + "' unexpected") 
             return False
     return True
+
+def _print_command(command_name : str, description : str, args_name : list[str], args_desc : list[str], example : str) -> None:
+    print('Command: ' + command_name)
+    print(description)
+    n_args : int = len(args_name)
+    for i in range(n_args):
+        arg_name : str = args_name[i]
+        arg_desc : str = args_desc[i]
+        print('\t' + arg_name + ' - ' + arg_desc) 
+    print('Example:\n')
+    print(example)
+    print('\n' * 2)
+
+def _print_help_header() -> None:
+    print("\n" * 2)
+    print("################################################################")
+    print("\t\t\t ONNXDBG INFO")
+    print("################################################################")
+    print("\n" * 2)
+
+def _print_help_tail() -> None:
+    print("\n" * 2)
+    print("################################################################")
+    print("\n" * 2)
--- onnxdbg/onnxdbg.py
+++ onnxdbg/onnxdbg.py
@@ -1,4 +1,4 @@
-from .commands import onnxdbg_copy, onnxdbg_subgr, onnxdbg_inferdbg
+from .commands import onnxdbg_copy, onnxdbg_subgr, onnxdbg_inferdbg, onnxdbg_help
 
 def onnxdbg(command : str, **kwargs) -> None:
     if command == "copy":
@@ -7,5 +7,7 @@ def onnxdbg(command : str, **kwargs) -> None:
         onnxdbg_subgr(**kwargs)
     elif command == "inferdbg":
         onnxdbg_inferdbg(**kwargs)
+    elif command == "help":
+        onnxdbg_help(**kwargs)
     else:
         print("Command '" + command + "' not existing")
