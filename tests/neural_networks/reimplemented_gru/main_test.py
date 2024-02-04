e6e261440426b9e8ef21cf1f98c55ef3797d6463
alecerio <alessandrocerioliphd@gmail.com>
2024-02-05 00:07:09 +0100

    add test for reimplemented_gru_2

--- tests/neural_networks/reimplemented_gru/main_test.py
+++ tests/neural_networks/reimplemented_gru/main_test.py
@@ -97,6 +97,50 @@ class TestReimplementedGRU(unittest.TestCase):
         # compare results
         compare_results(self, output_onnx, output_c, "ONNX", "C", 1e-6)
 
+    def test_02(self):
+
+        print_test_header("REIMPLEMENTED GRU 02 ONNX", 4)
+
+        # init config file
+        name : str = CompilerConfig()['name']
+        output_path : str = CompilerConfig()['output_path']
+        test_path : str = CompilerConfig()['test_path'] + 'neural_networks/reimplemented_gru/'
+        temp_path : str = CompilerConfig()['temp_path']
+        path_onnx = test_path + 'gru_reimplemented_2.onnx'
+
+        # inference onnxruntime
+        x_data =  np.ones((1, 3), dtype=np.float32)
+        hidden_data =  np.zeros((1, 3, 4), dtype=np.float32)
+        input_data = [x_data, hidden_data]
+        [outputs_onnx, outputs_shape_onnx] = inference_onnx_runtime(path_onnx, input_data)
+        output_onnx = outputs_onnx[0]
+        output_shape_onnx = outputs_shape_onnx[0]
+
+        # run compiler
+        run(CompilerConfig(), framework='onnx', path=path_onnx)
+        
+        return 
+        # create test main.c
+        create_main_c(test_path, output_path, name)
+        
+        # run command
+        try:
+            subprocess.run(["bash", test_path + "build.sh", name, output_path], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error: {e}")
+
+        # read inferred output shape
+        output_shape_c = read_inferred_output_shape(temp_path)
+        
+        # read c output
+        output_c = read_output_c(output_path)
+
+        # compare shape
+        compare_shape(self, output_shape_onnx, output_shape_c, "ONNX", "C")
+
+        # compare results
+        compare_results(self, output_onnx, output_c, "ONNX", "C", 1e-6)
+
 def run_tests():
     curr_file = os.path.abspath(__file__)
     curr_path = os.path.dirname(curr_file)
