8f6fc537ffbf41d2c33b6c0f43b3845c83fa9f0c
alecerio <alessandrocerioliphd@gmail.com>
2024-02-06 22:39:08 +0100

    implementation experiments for neuralcasting

---
+++ experiments/gru/neuralcasting/dummy.h
@@ -0,0 +1,9 @@
+// INCLUDE
+
+#include <math.h>
+#include <stdint.h>
+
+
+typedef float float32_t;
+
+void run_inference(float32_t* tensor_onnxGemm_0, float32_t* tensor_onnxMatMul_1, float32_t* tensor_37);
