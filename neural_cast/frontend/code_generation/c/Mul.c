db348fe3743a7107d379548d3350477551efbfec
alecerio <alessandrocerioliphd@gmail.com>
2024-03-04 19:28:03 +0100

    implementation benchmark Mul

--- neural_cast/frontend/code_generation/c/Mul.c
+++ neural_cast/frontend/code_generation/c/Mul.c
@@ -7,9 +7,17 @@ $OUTPUT_TYPE tensor_$OUTPUT_NAME[$INPUT_SIZE];
 #undef CONNECTED_OUTPUT
 #endif
 
+#ifdef COMPILER_BENCHMARK
+neuralcasting_start_benchmark = omp_get_wtime();
+#endif
+
 $FOR_LOOPS_BEGIN
 tensor_$OUTPUT_NAME[$INDEX_TOT] = tensor_$INPUT1_NAME[$INDEX_1] * tensor_$INPUT2_NAME[$INDEX_2];
 $FOR_LOOPS_END
+
+#ifdef COMPILER_BENCHMARK
+BENCHMARK("tensor_$NAME", $NFLOPS)
+#endif
 
 #ifdef COMPILER_DEBUG
 printf("----------------- DEBUG OUTPUT $NAME -----------------\n");
