qmatmul and qprelu scaling factor fixed point 16

tests/qlinearmatmul/test_matmul.py
@@ -11,8 +11,8 @@ def test_matmul():
     y_float = matmul_float_model(a, b)
     
     # quantization data
-    Q = 31
-    QS = 20
+    Q = 15
+    QS = 0
     sa, za = compute_s_z(a)
     sb, zb = compute_s_z(b)
     sy, zy = compute_s_z(y_float)
@@ -29,8 +29,8 @@ def test_matmul():
     assert np.allclose(y_rec2, y_float, atol=3e-1)
 
     # compute quantized model fixed point c
-    y_rec3 = matmul_quant_fixed_point_c(a, b, 2, 2, 2, sfxa, za, sfxb, zb, sfxy, zy, Q, QS)
-    assert np.allclose(y_rec3, y_float, atol=3e-1)
+    y_rec3 = matmul_quant_fixed_point_c(a, b, 2, 2, 2, sfxa, za, sfxb, zb, sfxy, zy, Q)
+    assert np.allclose(y_rec3, y_float, atol=7e-1)
 
 def matmul_float_model(a: np.array, b: np.array) -> np.array:
     y = np.matmul(a, b)
@@ -72,7 +72,7 @@ def matmul_quant_fixed_point(a: np.array, b: np.array, sfxa: int, zfxa: int, sfx
 
     return y_rec
 
-def matmul_quant_fixed_point_c(a: np.array, b: np.array, M: int, N: int, K: int, sfxa: int, za: int, sfxb: int, zb: int, sfxc: int, zc: int, Q: int, QS: int):
+def matmul_quant_fixed_point_c(a: np.array, b: np.array, M: int, N: int, K: int, sfxa: int, za: int, sfxb: int, zb: int, sfxc: int, zc: int, Q: int):
     aq = quantize_linear_fixed_point(a, sfxa, za, Q)
     bq = quantize_linear_fixed_point(b, sfxb, zb, Q)
 
@@ -86,6 +86,7 @@ def matmul_quant_fixed_point_c(a: np.array, b: np.array, M: int, N: int, K: int,
     cname = "main"
     exename = "test"
     outname = "out"
+    acctype = "int32_t"
 
     main = f"""
 #include "ncast_lib.h"
@@ -96,7 +97,7 @@ int main() {{
     int8_t bq[{size}] = {{ {bq_str} }};
 
     int8_t cq[{size}];
-    NC_QLINMATMUL(aq,bq,cq,{M},{N},{K},{sfxa},{sfxb},{za},{zb},{sfxc},{zc},{Q},{QS})
+    NC_QLINMM_FXS8(aq,bq,cq,{M},{N},{K},{sfxa},{sfxb},{za},{zb},{sfxc},{zc},{Q},{acctype})
 
     NC_OUTTNS("{TEST_TMP_PATH}/{outname}.txt",cq,{size},"%d");
 

tests/qlinearmul/test_qlinearmul.py
@@ -5,8 +5,8 @@ from config.config import TEST_TMP_PATH, LIB_DIR
 
 def test_qlinearmul():
     # compute floating point model
-    a = np.array([-0.2, -0.1, 0.0, 0.1, 0.2], dtype=np.float32)
-    b = np.array([-0.6, -0.3, 0.0, 0.3, 0.6], dtype=np.float32)
+    a = np.array([-1.2, -1.1, 1.0, -0.1, 1.2], dtype=np.float32)
+    b = np.array([-2.6, -1.3, 0.0, 3.3, 0.6], dtype=np.float32)
     c = linearmul_float(a, b)
 
     # compute quantization data
@@ -20,15 +20,15 @@ def test_qlinearmul():
 
     # compute quantized model
     c_rec1 = linearmul_quant_float(a, b, sa, za, sb, zb, sc, zc)
-    assert np.allclose(c_rec1, c, atol=1e-1)
+    assert np.allclose(c_rec1, c, atol=2e0)
 
     # compute quantized model fixed point
     c_rec2 = linearmul_quant_fixed_point(a, b, sfxa, zfxa, sfxb, zfxb, sfxc, zfxc, Q)
-    assert np.allclose(c_rec2, c, atol=1e-1)
+    assert np.allclose(c_rec2, c, atol=2e0)
 
     #compute quantized model fixed point c
-    c_rec3 = linearnul_quant_fixed_point_c(a, b, sfxa, zfxa, sfxb, zfxb, sfxc, zfxc, Q)
-    assert np.allclose(c_rec3, c, atol=1e-1)
+    c_rec3 = linearmul_quant_fixed_point_c(a, b, sfxa, zfxa, sfxb, zfxb, sfxc, zfxc, Q)
+    assert np.allclose(c_rec3, c, atol=2e0)
 
 def linearmul_float(a: np.array, b: np.array) -> np.array:
     c = np.multiply(a, b)
@@ -60,7 +60,7 @@ def linearmul_quant_fixed_point(a: np.array, b: np.array, sfxa: int, zfxa: int, 
     c_rec = dequentize_linear_fixed_point(a3, sfxc, zfxc, Q)
     return c_rec
 
-def linearnul_quant_fixed_point_c(a: np.array, b: np.array, sfxa: int, za: int, sfxb: int, zb: int, sfxc: int, zc: int, Q: int):
+def linearmul_quant_fixed_point_c(a: np.array, b: np.array, sfxa: int, za: int, sfxb: int, zb: int, sfxc: int, zc: int, Q: int):
     aq = quantize_linear_fixed_point(a, sfxa, za, Q)
     bq = quantize_linear_fixed_point(b, sfxb, zb, Q)
 

tests/qlinearprelu/test_qlinearprelu.py
@@ -16,7 +16,7 @@ def test_prelu2():
     assert np.allclose(y_torch, y_float, atol=1e-6)
 
     # compute quantization data
-    Q = 31
+    Q = 15
     sx, zx = compute_s_z(x)
     sy, zy = compute_s_z(y_float)
     sfxx, zfxx = compute_sfx_zfx(sx, zx, Q)
@@ -81,6 +81,7 @@ def prelu_quant_fixed_point_c(x: np.array, w: np.float32, sfxx: int, zx: int, sf
     cname = "main"
     exename = "test"
     outname = "out"
+    acctype = "int32_t"
 
     main = f"""
 #include "ncast_lib.h"
@@ -90,7 +91,7 @@ int main() {{
     int8_t xq[{size}] = {{ {xq_str} }};
 
     int8_t yq[{size}];
-    NC_QLPRELU_FXS8(xq,{wfx},yq,{size},{sfxx},{zx},{sfxy},{zy},{Q});
+    NC_QLPRELU_FXS8(xq,{wfx},yq,{size},{sfxx},{zx},{sfxy},{zy},{Q},{acctype});
 
     NC_OUTTNS("{TEST_TMP_PATH}/{outname}.txt",yq,{size},"%d");
 