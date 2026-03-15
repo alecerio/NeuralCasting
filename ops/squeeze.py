156b7c9f032ff050b035d28ba21a9a4d36a0cffc
alecerio <alessandrocerioliphd@gmail.com>
2026-03-15 13:29:00 +0100

    update squeeze

--- ops/squeeze.py
+++ ops/squeeze.py
@@ -2,9 +2,10 @@
 from ops.ncast_op import NCastOp, NCastOutputDict
 from config.config import NCastConfig
 from typing import List
-from common.common import retrieve_input
+from common.common import retrieve_input, get_init_data
 import onnx
 from common.common import not_implemented_feature_exception
+import numpy as np
 
 class Squeeze(NCastOp):
     def __init__(self, onnx_unit):
@@ -14,13 +15,23 @@ class Squeeze(NCastOp):
         result = retrieve_input(graph, ops, self.onnx_unit.input[0])
         shape: List[int] = result[0]
         dtype: int = result[1]
-        i = 0
-        if shape != []:
-            while shape[i] == 1 and i < len(shape):
-                i = i+1
-            new_shape = shape[i:]
+
+        axes = get_init_data(graph, self.onnx_unit.input[1])
+        if axes != None:
+            shape_np = np.array(shape)
+            axes_np = np.array(axes)
+            mask = np.ones(len(shape_np), dtype=bool)
+            mask[axes_np] = False
+            new_shape_np = shape_np[mask]
+            new_shape = new_shape_np.astype(int).tolist()
         else:
-            new_shape = []
+            i = 0
+            if shape != []:
+                while shape[i] == 1 and i < len(shape):
+                    i = i+1
+                new_shape = shape[i:]
+            else:
+                new_shape = []
         self.out_dict[self.onnx_unit.output[0]] = NCastOutputDict(new_shape, dtype)
 
     def emit_includes(self, config: NCastConfig, graph: onnx.onnx_ml_pb2.GraphProto, ops: List[NCastOp]) -> List[str]:
